47cab25ace779882c9b3a3ecd34a99c80a23fff7
JakeServin <jakeservin@gmail.com>
2022-04-23 09:45:10 -0700

    created scripts.js

---
+++ 8ball/scripts.js
@@ -0,0 +1,37 @@
+let responses = [
+    'It is certain.',
+    'It is decidely so.', 'Without a doubt.',
+    'Yes definitely',
+    'You may rely on it.',
+    'As I see it, yes.',
+    'Most likely.',
+    'Outlook good',
+    'Yes.',
+    'Signs point to yes.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'Cannot predict now.',
+    'Concentrate and ask again',
+    "Don't count on it.",
+    'My reply is no.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.'
+];
+
+// Generate random number between 2 ints 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Get random response 
+function getResponse() {
+    return responses[getRandomIntInclusive(0, responses.length-1)];
+}
+
+
+
+
